7ad6aa3adb4e694fcd5e48a9e35f10cdae6db82b
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2025-06-22 02:34:01 +0300

    footer and Policy components add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Policy from "./components/Policy";
 
 function App() {
   return (
     <>
       <Header />
       <h1>Hello!</h1>
+      <Policy/>
+      <Footer/>
     </>
   );
 }
