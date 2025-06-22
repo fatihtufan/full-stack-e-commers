5bfadc851d9455917115a83a8ff7a8dedb2fd232
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2025-06-22 15:49:57 +0300

    Created HomePage Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,13 @@
-import Header from "./components/Layout/Header/Header";
-import Footer from "./components/Layout/Footer/Footer";
-import Policy from "./components/Layout/Policy/Policy";
-import Sliders from "./components/Slider/Sliders";
-import Categories from "./components/Categories/Categories";
-import Products from "./components/Products/Products";
+import React from "react";
 import "./App.css";
-import Campaigns from "./components/Campaigns/Campaigns";
-import Blogs from "./components/Blogs/Blogs";
-import Brands from "./components/Brands/Brands";
-import CampaignSingle from "./components/CampaignSingle/CampaignSingle";
+
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Sliders />
-      <Categories />
-      <Products />
-      <Campaigns />
-      <Products />
-      <Blogs />
-      <Brands />
-      <CampaignSingle/>
-      <Policy />
-      <Footer />
-    </>
+    <React.Fragment>
+      <HomePage />
+    </React.Fragment>
   );
 }
 
