a0ef9b42634b60e27ee756c9b988ba68e7eea9e9
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2025-06-22 15:27:12 +0300

    Blog component add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Categories from "./components/Categories/Categories";
 import Products from "./components/Products/Products";
 import "./App.css";
 import Campaigns from "./components/Campaigns/Campaigns";
+import Blogs from "./components/Blogs/Blogs";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
       <Categories />
       <Products />
       <Campaigns />
+      <Products />
+      <Blogs />
       <Policy />
       <Footer />
     </>
