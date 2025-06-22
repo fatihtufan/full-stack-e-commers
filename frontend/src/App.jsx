487553d1a5342c38a15a7f506868ce4e5db4ca38
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2025-06-22 03:24:08 +0300

    Products components add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import "./App.css";
 import Header from "./components/Layout/Header/Header";
 import Footer from "./components/Layout/Footer/Footer";
 import Policy from "./components/Layout/Policy/Policy";
 import Sliders from "./components/Slider/Sliders";
 import Categories from "./components/Categories/Categories";
+import Products from "./components/Products/Products";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
       <Sliders />
-      <Categories/>
+      <Categories />
+      <Products />
       <Policy />
       <Footer />
     </>
