a34d97800771cf7aa03218392aa6d184515b21cc
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2025-06-22 03:06:59 +0300

    Categories components add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import Header from "./components/Layout/Header/Header";
 import Footer from "./components/Layout/Footer/Footer";
 import Policy from "./components/Layout/Policy/Policy";
-import Sliders from "./components/Layout/Slider/Sliders";
+import Sliders from "./components/Slider/Sliders";
+import Categories from "./components/Categories/Categories";
 
 function App() {
   return (
     <>
       <Header />
       <Sliders />
+      <Categories/>
       <Policy />
       <Footer />
     </>
